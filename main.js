0de360efc2a399fe83623389130cc130694d8696
alec azimuth <azidyn@protonmail.com>
2020-06-15 21:13:25 +0100

    prevent memory overgrowth

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
+
 const WebSocketClient   = require('./ws/WebsocketClient');
-const Aggregate         = require('./Aggregate');
+const Aggregate         = require('./lib/Aggregate');
 const config            = require('./config');
 
 const wsc = new WebSocketClient();
@@ -10,7 +11,7 @@ const aggregate = new Aggregate({ resolution: 1 * 1000 * 60 });
 wsc.open( config.BITMEX_WSS );
 
 wsc.onopen = ( e ) => { 
-    console.log( "BitMEX: Connected. Receiving data..." );
+    console.log( "BitMEX connected. Streaming data..." );
     wsc.send( config.BITMEX_TOPIC );
 }
 
@@ -18,7 +19,7 @@ wsc.onmessage = async ( data, flags, number ) => {
 
     let frame = JSON.parse( data );
 
-    // ignore any crap
+    // ignore any weirdo crap
     if ( !frame || !frame.table || !frame.data || !frame.action || !frame.action=='insert' || 
          !frame.table == 'trade' || !Array.isArray( frame.data ) || !frame.data.length )
         return;
@@ -32,3 +33,12 @@ wsc.onmessage = async ( data, flags, number ) => {
     }
 
 }
+
+aggregate.on( 'initialized', () => console.log('Partial bar completed, waiting for next full bar...') );
+
+aggregate.on( 'bar', bar => {
+    
+    // Print OHLCV and open time
+    console.log( bar );
+
+});
