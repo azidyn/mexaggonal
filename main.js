973c774dc24b6b46e502e8954d694cc2d26b12f2
alec azimuth <azidyn@protonmail.com>
2020-06-15 23:03:16 +0100

    refactored to single object call

--- main.js
+++ main.js
@@ -1,44 +1,23 @@
 
-const WebSocketClient   = require('./ws/WebsocketClient');
 const Aggregate         = require('./lib/Aggregate');
 const config            = require('./config');
-
-const wsc = new WebSocketClient();
 
 const ONE_MINUTE    = 1 * 1000 * 60;
 const FIVE_MINUTES  = 5 * 1000 * 60;
 
 // Assumes one hour integer divisible and 00 seconds aligned
-const aggregate = new Aggregate({ resolution: ONE_MINUTE });
+const aggregate = new Aggregate({     
+    url: config.BITMEX_WSS,
+    topic: config.BITMEX_TOPIC,
+    resolution: ONE_MINUTE 
+});
 
-// see config.js for live bitmex const
-wsc.open( config.BITMEX_WSS_TESTNET );
 
-wsc.onopen = ( e ) => { 
-    console.log( "BitMEX connected. Streaming data..." );
-    wsc.send( config.BITMEX_TOPIC );
-}
-
-wsc.onmessage = async ( data, flags, number ) => {
-
-    let frame = JSON.parse( data );
-
-    // ignore any weirdo crap
-    if ( !frame || !frame.table || !frame.data || !frame.action || !frame.action=='insert' || 
-         !frame.table == 'trade' || !Array.isArray( frame.data ) || !frame.data.length )
-        return;
-
-    for ( let d of frame.data )
-    {
-        if ( !d.price || !d.timestamp || !d.size ) continue;
-
-        aggregate.add( d.timestamp, d.price, d.size );
-
-    }
-
-}
+aggregate.start();
 
 // Emitted output from aggregation...
+
+aggregate.on('connected', () => console.log('BitMEX connected.') );
 
 aggregate.on( 'initialized', () => console.log('Partial bar completed, waiting for next full bar...') );
 
