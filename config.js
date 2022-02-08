2125d14f632bd21619f90725d0887433383e6364
removedporn <86824510+removedporn@users.noreply.github.com>
2022-02-08 20:40:04 +0800

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-
-    BITMEX_WSS: 'wss://www.bitmex.com/realtime',
-    BITMEX_WSS_TESTNET: 'wss://testnet.bitmex.com/realtime',
-    BITMEX_TOPIC: `{ "op": "subscribe", "args":["trade:XBTUSD"] }`
-}
